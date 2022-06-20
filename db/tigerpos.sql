c93ef9c378c0e7589ad98336b98de1c58d4faa61
asamamun <asamamun.web@gmail.com>
2022-06-20 18:27:39 +0600

    customer table deleted field added

--- db/tigerpos.sql
+++ db/tigerpos.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2022 at 11:23 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
+-- Generation Time: Jun 20, 2022 at 06:25 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,18 +34,24 @@ CREATE TABLE `customers` (
   `mobile` varchar(20) NOT NULL,
   `address` varchar(255) NOT NULL,
   `expense` decimal(20,2) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT current_timestamp()
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `deleted` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `customers`
 --
 
-INSERT INTO `customers` (`id`, `name`, `email`, `mobile`, `address`, `expense`, `created`) VALUES
-(1, 'Shariful Islam', 'php.shariful@gmail.com', '01746959343', 'Mirpur 13, Dhaka-1216', '0.00', '2022-06-19 21:08:52'),
-(2, 'Tamimul Islam', 'tamimislam732@gmail.com', '01911151732', 'Savar, Dhaka', '0.00', '2022-06-19 21:08:52'),
-(3, 'Syed Zayed Hosssain', 'php.zayed@gmail.com', '01629999666', 'Manikdi, Dhaka Cantonment, Dhaka-1206', '0.00', '2022-06-19 21:10:34'),
-(4, 'Tasnim Al Rahman', 'tasnim333@gmail.com', '01712345678', 'Mirpur, Dhaka-1216', '0.00', '2022-06-19 21:10:34');
+INSERT INTO `customers` (`id`, `name`, `email`, `mobile`, `address`, `expense`, `created`, `deleted`) VALUES
+(1, 'Shariful Islam', 'php.shariful@gmail.com', '01746959343', 'Mirpur 13, Dhaka-1216', '0.00', '2022-06-19 21:08:52', NULL),
+(2, 'Tamimul Islam', 'tamimislam732@gmail.com', '01911151732', 'Savar, Dhaka', '0.00', '2022-06-19 21:08:52', NULL),
+(3, 'Syed Zayed Hosssain', 'php.zayed@gmail.com', '01629999666', 'Manikdi, Dhaka Cantonment, Dhaka-1206', '0.00', '2022-06-19 21:10:34', NULL),
+(4, 'Tasnim Al Rahman', 'tasnim333@gmail.com', '01712345678', 'Mirpur, Dhaka-1216', '0.00', '2022-06-19 21:10:34', NULL),
+(5, 'ASA Al-Mamun', 'asamamun.web@gmail.com', '01911039525', 'Dhaka', '123.00', '2022-06-20 10:18:12', NULL),
+(6, 'admin123 admin', 'test@gmail.com', '0195465656', 'Taltola', '666.00', '2022-06-20 10:19:01', '2022-06-20 00:25:15'),
+(8, 'ASA Al-Mamun456', 'asamamun.web456@gmail.com', '01911039527', 'Dhaka', '777777.00', '2022-06-20 10:20:56', NULL),
+(9, 'Xioami MI', 'mi@mi.com', '0123123123', 'Dhaka', '333.00', '2022-06-20 10:25:26', NULL),
+(10, 'admin1277', 'admin@idbbisew.com', '01911039529', 'Taltola', '111111.00', '2022-06-20 10:52:10', '2022-06-20 00:25:20');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +72,7 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
