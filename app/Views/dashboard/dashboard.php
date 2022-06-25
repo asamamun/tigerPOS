f5f9437783c9f78dd92b3aa56ce7bfdf1a295597
Md Shariful Islam <shaarifultarsi51@gmail.com>
2022-06-25 23:32:32 +0600

    Dashboard filter button modified

--- app/Views/dashboard/dashboard.php
+++ app/Views/dashboard/dashboard.php
@@ -1,7 +1,32 @@
 <?= $this->extend('layouts/default'); ?>
 
 <?= $this->section('contents'); ?>
-<h1>Dashboard</h1>
+<div class="row">
+    <div class="col-6">
+        <h1>Dashboard</h1>
+    </div>
+    <div class="col-6">
+        <div class="dropdown ">
+            <button class="btn btn-light dropdown-toggle position-absolute top-50 end-0 translate-middle-y mt-3 " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Filter By Date
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">Today</a></li>
+                <li><a class="dropdown-item" href="#">Yesterday</a></li>
+                <li><a class="dropdown-item" href="#">Last 7 Days</a></li>
+                <li><a class="dropdown-item" href="#">Last month</a></li>
+                <li><a class="dropdown-item" href="#">This month</a></li>
+                <li><a class="dropdown-item" href="#">Last 6 Month</a></li>
+                <li><a class="dropdown-item" href="#">This Year</a></li>
+                <li><a class="dropdown-item" href="#">last year</a></li>
+                <li><a class="dropdown-item" href="#">last Financial year</a></li>
+                <li><a class="dropdown-item" href="#">this Financial year</a></li>
+                <li><a class="dropdown-item" href="#">Custom Range</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
 <div class="row ">
     <div class="col-sm-3 btn btn-light">
         <div class="card">
