27034241a0906429c5d2f5b34dc7eba2fb1ba677
irinabbas <92804220+irinabbas@users.noreply.github.com>
2022-07-06 00:48:16 +0600

    totalsales counted

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,16 +3,27 @@
 namespace App\Controllers;
 
 use App\Models\UserModel;
+use App\Models\InvoiceModel;
 
 class Home extends BaseController
 {
+    public function __construct()
+    {
+        $this->db = \Config\Database::connect();
+    }
     public function index()
     {
         if ($this->checkauth()) {
             $u = new UserModel();
             $data['totalusers'] = $u->countAll();
-            //TODO: collect data from databae
-            $data['totalsales'] = 9999;
+            //total of grand total
+            $invoice = new InvoiceModel();
+		$result = $invoice->select('sum(grandtotal) as totalsales')->first();
+		$data['totalsales'] = $result['totalsales'];
+
+
+
+
             $data['totalpurchase'] = 9999;
             $data['totalexpense'] = 9999;
             $data['totalorder'] = 9999;
